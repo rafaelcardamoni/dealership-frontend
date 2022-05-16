51e56b917a0affb260fb3213b7ba21c1ee966a82
Rafael Cardamoni <rafael.cardamoni@gmail.com>
2022-05-16 14:35:44 -0300

    added interface to context parameter on getServerSideApi

--- src/services/serverSideApi.ts
+++ src/services/serverSideApi.ts
@@ -1,7 +1,18 @@
 import { parseCookies } from 'nookies';
 import axios from 'axios';
+import * as next from 'next';
+import * as cookie from 'cookie';
 
-export function getServerSideApi(ctx?: any) {
+export function getServerSideApi(
+  ctx?:
+    | Pick<next.NextPageContext, 'req'>
+    | {
+        req: next.NextApiRequest;
+      }
+    | null
+    | undefined,
+  options?: cookie.CookieParseOptions
+) {
   const { ['nextauth.token']: token } = parseCookies(ctx);
 
   const api = axios.create({
