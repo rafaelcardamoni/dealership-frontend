860d0a99c4a976c4493fc1255911af24d8f72ede
Rafael Cardamoni <rafael.cardamoni@gmail.com>
2022-05-28 20:00:47 -0300

    fixed location link

--- src/components/FindUs/index.tsx
+++ src/components/FindUs/index.tsx
@@ -10,7 +10,7 @@ export function FindUs({ separator }: SeparatorProps) {
   return (
     <div className={styles.container}>
       {separator ? <MenuSeparatorVertical /> : ''}
-      <a href="https://www.google.com/maps/dir//manocar/@-23.5234253,-46.6571677,13z/data=!4m8!4m7!1m0!1m5!1m1!1s0x94cef7baf458c3c5:0xd3010fafce25a1fb!2m2!1d-46.6118346!2d-23.4967696">
+      <a href="https://www.google.com/maps/">
         <BiMap size={23} style={{ verticalAlign: 'bottom' }} />
         <span>Localização</span>
       </a>
